f6c3c8deb41bc43882c7d08134b070b8f80f7a1b
Strider <mezentsev.k@gmail.com>
2023-09-28 20:16:24 +0800

    Задача 5 выполнена

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Память");
+    public static void changeValue(int value) {
+        value = 22;
     }
-}
+    public static void main (String[]args){
+        System.out.println("Память");
+        System.out.println("___________________");
+        System.out.println("Задача №5");
+        int value = 33;
+        changeValue(value);
+        System.out.println(value);
+
+        }
+    }
