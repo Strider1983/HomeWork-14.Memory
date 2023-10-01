102b6e69705a3a43a3163171afb12afc9bcfb0bd
Strider <mezentsev.k@gmail.com>
2023-10-01 11:06:53 +0800

    Задача 6 выполнена

---
+++ src/Task5.java
@@ -0,0 +1,12 @@
+public class Task5 {
+    public static void changeValue(int value) {
+        value = 22;
+    }
+    public static void main (String[]args){
+        int value = 33;
+        System.out.println("Start value - " + value);
+        changeValue(value);
+        System.out.println("Nev value - " + value);
+
+        }
+    }
---
+++ src/Task6.java
@@ -0,0 +1,12 @@
+public class Task6 {
+    public static void changeValue(Integer value) {
+        value = 22;
+    }
+    public static void main (String[]args){
+        Integer value = 33;
+        System.out.println("Start value - " + value);
+        changeValue(value);
+        System.out.println("Nev value - " + value);
+
+        }
+    }
