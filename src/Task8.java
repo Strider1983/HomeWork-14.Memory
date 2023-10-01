68fc874e430a664c617d534a44e929c0820c8dc7
Strider <mezentsev.k@gmail.com>
2023-10-01 11:23:16 +0800

    Задача 8 выполнена

---
+++ src/Task8.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class Task8 {
+    public static void changeValue(Integer[] value) {
+        value[0] = 99;
+    }
+    public static void main (String[]args){
+        Integer [] value = new Integer[] {3, 4};
+        System.out.println("Start value - " + Arrays.toString(value));
+        changeValue(value);
+        System.out.println("Nev value - " + Arrays.toString(value));
+        }
+    }
